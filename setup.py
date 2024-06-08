471a77286a8457a1205bc06308ef7b849aa9be9f
Cody Christ <cchrist1210@gmail.com>
2024-06-08 10:24:58 -0700

    Add setup.py with the project intialization settings

---
+++ setup.py
@@ -0,0 +1,21 @@
+
+import setuptools
+
+with open('README.md', encoding='utf-8') as file:
+    readme = file.read()
+
+setuptools.setup(
+    name='fastapi-auth0',
+    version='0.5.0',
+    description='Easy auth0.com integration for FastAPI',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url='https://github.com/super999christ/fastapi-auth0',
+    author='Cody Christ',
+    license='MIT',
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    package_data={'': ['py.typed']},
+    python_requires='>=3.7',
+    install_requires=['fastapi>=0.60.0', 'python-jose>=3.2.0']
+)
